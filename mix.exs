7368361058015df71c412b8f5ec10dd1849b8ede
Victor campos <victor.campos@visagio.com>
2016-01-03 12:06:16 -0200

    update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ytx.Mixfile do
 
   def project do
     [app: :ytx,
-     version: "0.0.4",
+     version: "0.0.5",
      description: "Youtube API Client for Elixir",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
